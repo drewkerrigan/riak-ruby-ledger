9adf0c3e599e31b3dbaf76baf184413bb719d9d9
drewkerrigan <dkerrigan@basho.com>
2013-12-11 01:56:32 -0800

    untested transaction ledger counter functionality

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -1,5 +1,54 @@
 require "ledger/version"
 
-module Ledger
-  # Your code goes here...
+module Riak::Ledger
+  attr_accessor :actor, :client, :bucket, :key, :counter
+
+  def initialize(actor, client, bucket, key)
+    self.actor = actor
+    self.client = client
+    self.bucket = bucket
+    self.key = key
+    self.counter = TPNCounter.new()
+  end
+
+  def self.find(actor, client, bucket, key)
+    obj = client[bucket].get_or_new(key)
+    return if obj.nil?
+
+    candidate = new(client, bucket, key)
+
+    if obj.siblings.length > 1
+      index_obj.siblings.each do | o |
+        unless o.raw_data.nil? or o.raw_data.empty?
+          candidate.counter.merge(actor, TPNCounter.from_json(o.raw_data))
+        end
+      end
+
+      resolved_obj = client[bucket].new(key)
+      resolved_obj.vclock = obj.vclock
+
+      # previous content type was mulitpart/mixed, needs to change
+      resolved_obj.content_type = 'application/json'
+      resolved_obj.raw_data = candidate.counter.to_json
+      resolved_obj.store(options={:returnbody => false})
+    elsif !obj.raw_data.nil?
+      candidate.counter.merge(actor, TPNCounter.from_json(obj.raw_data)))
+    end
+  end
+
+  def value
+    counter.value
+  end
+
+  def increment(transaction, value)
+    counter.increment(actor, transaction, value)
+  end
+
+  def save()
+    object = self.bucket.new(key)
+    object.content_type = 'application/json'
+    object.raw_data = counter.to_json
+
+    object.store(options={:returnbody => false})
+  end
 end
---
+++ lib/transaction_gcounter.rb
@@ -0,0 +1,58 @@
+require 'set'
+
+module Riak::Ledger
+  class TGCounter
+    attr_accessor :counts
+    def initialize
+      self.counts = Hash.new()
+    end
+
+    def to_json
+      {
+          type: 'TGCounter',
+          c: counts
+      }.to_json
+    end
+
+    def self.from_json(json)
+      h = JSON.parse json
+      raise ArgumentError.new 'unexpected type field in JSON' unless h['type'] == 'TGCounter'
+
+      gc = new
+      gc.counts = h['c']
+      return gc
+    end
+
+    def increment(actor, transaction, value)
+      counts[actor] = Hash.new() unless counts[actor]
+      counts[actor][transaction] = value
+    end
+
+    def value
+      transactions = Hash.new()
+
+      counts.values.each do |t,v|
+        transactions[t] = v
+      end
+
+      transactions.values.inject(0, &:+)
+    end
+
+    # Cannot modify other actors' sets because of possible simultaneous merges
+    def merge(actor, other)
+      new_keys = Set.new
+      counts[actor] = Hash.new unless counts[actor]
+      new_keys.merge counts[actor].keys
+      new_keys.merge other.counts[actor].keys if other.counts[actor]
+
+      new_keys.each do |k|
+        counts[actor][k] = [counts[actor][k], other.counts[actor][k]].max
+      end
+
+      actor_total = counts[actor].values.inject(0, &:+)
+
+      counts[actor] = Hash.new()
+      counts[actor]["total"] = actor_total
+    end
+  end
+end
